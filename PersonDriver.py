be225dac2ac2674c7149dc2e33fc2385c5bcedbc
gloxiinia <91533469+gloxiinia@users.noreply.github.com>
2021-12-14 13:35:17 +0700

    Update PersonDriver.py

--- PersonDriver.py
+++ PersonDriver.py
@@ -1,4 +1,24 @@
 from PersonDriver import Person
+from PersonDriver import Teacher
+from PersonDriver import Student
 
-t = Person()
-t.Set
+x = Person()
+x.SetAddress('Promenade')
+x.SetName('Emily')
+print(x.__str__()+ '\n')
+
+y = Teacher()
+y.SetAddress('Outskirts')
+y.SetName('Valen')
+y.SetCourses(['Economics', 'Physics', 'Math', 'Biology'])
+y.RemoveCourse('Physics')
+print(y.__str__() + '\n')
+
+z = Student()
+z.SetAddress('Capitol')
+z.SetName('Gunther')
+z.SetGrades([85, 87, 90, 100])
+z.SetCourses(['Classical Arts', 'Musical Theory', 'Graphic Design'])
+z.SetNumCourses(4)
+z.AddCourseGrade('Classical Arts', 85)
+print(z.__str__())
